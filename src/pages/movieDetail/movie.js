bf77cb7483b82744329eb5fa6bc909b9048e1b5a
Abhimanyu Kumar <kabhimanyu656@gmail.com>
2023-01-16 18:08:36 +0530

    Production

--- src/pages/movieDetail/movie.js
+++ src/pages/movieDetail/movie.js
@@ -7,17 +7,16 @@ const Movie = () => {
   const { id } = useParams();
 
   useEffect(() => {
+    const getData = () => {
+      fetch(
+        `https://api.themoviedb.org/3/movie/${id}?api_key=4e44d9029b1270a757cddc766a1bcb63&language=en-US`
+      )
+        .then((res) => res.json())
+        .then((data) => setMovie(data));
+    };
     getData();
     window.scrollTo(0, 0);
   }, []);
-
-  const getData = () => {
-    fetch(
-      `https://api.themoviedb.org/3/movie/${id}?api_key=4e44d9029b1270a757cddc766a1bcb63&language=en-US`
-    )
-      .then((res) => res.json())
-      .then((data) => setMovie(data));
-  };
 
   return (
     <div className="movie">
