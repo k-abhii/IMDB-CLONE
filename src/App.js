8ca2ae62a9bb9d0c10fd1f2728e1583ae3ad5bc3
Abhimanyu Kumar <kabhimanyu656@gmail.com>
2023-01-16 09:23:26 +0530

    IMDB CLONE COMPLETED COROUSEL AND SEKELTON REACT LOADING USED

--- src/App.js
+++ src/App.js
@@ -1,6 +1,34 @@
+// import "./App.css";
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import Header from "./components/header/Header";
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <Router>
+//         <Header />
+//         <Routes>
+//           <Route index element={<h1>Hello World2</h1>}></Route>
+//           <Route path="movie/:id" element={<h1>Movie Detail Page</h1>}></Route>
+//           <Route
+//             path="movies/:type"
+//             element={<h1>Movies List Page</h1>}
+//           ></Route>
+//           <Route path="/*" element={<h1>Error Page </h1>}></Route>
+//         </Routes>
+//       </Router>
+//     </div>
+//   );
+// }
+
+// export default App;
+
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/header/Header";
+import Home from "./pages/home/home";
+import MovieList from "./components/movieList/movieList";
+import Movie from "./pages/movieDetail/movie";
 
 function App() {
   return (
@@ -8,13 +36,10 @@ function App() {
       <Router>
         <Header />
         <Routes>
-          <Route index element={<h1>Hello World2</h1>}></Route>
-          <Route path="movie/:id" element={<h1>Movie Detail Page</h1>}></Route>
-          <Route
-            path="movies/:type"
-            element={<h1>Movies List Page</h1>}
-          ></Route>
-          <Route path="/*" element={<h1>Error Page </h1>}></Route>
+          <Route index element={<Home />}></Route>
+          <Route path="movie/:id" element={<Movie />}></Route>
+          <Route path="movies/:type" element={<MovieList />}></Route>
+          <Route path="/*" element={<h1>Error Page</h1>}></Route>
         </Routes>
       </Router>
     </div>
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -41,6 +41,7 @@ const Header = () => {
           <img
             className="header__icon"
             src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/IMDB_Logo_2016.svg/2560px-IMDB_Logo_2016.svg.png"
+            alt="="
           />
         </Link>
         <Link to="/movies/popular" style={{ textDecoration: "none" }}>
